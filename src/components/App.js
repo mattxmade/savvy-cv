3ef8568c95b47c83efe13c66605c9ee71a7be9a7
Matt <97047080+mattxmade@users.noreply.github.com>
2022-08-08 19:11:45 +0100

    Add state, implement callbacks, render components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,44 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 
+import About from "./About";
+import Statement from "./Statement";
+import Education from "./Education";
+import Experience from "./Experience";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sections: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      sections: [<About />, <Statement />, <Education />, <Experience />],
+    });
+  }
+  componentDidUpdate(prevProps, prevState) {}
+
   render() {
-    return <h1>Hello World!</h1>;
+    return (
+      <div className="wrapper">
+        <header>
+          <h1>Savvy CV</h1>
+          <p>A CV building app</p>
+        </header>
+
+        <main>
+          <ul>
+            {this.state.sections.map((section, index) => (
+              <li key={index}>{section}</li>
+            ))}
+          </ul>
+        </main>
+      </div>
+    );
   }
 }
 
