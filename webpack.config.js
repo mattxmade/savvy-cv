8957b1b6956b5fd3b2752dcd82ecbe3079b0c9d9
Matt <97047080+mattxmade@users.noreply.github.com>
2022-08-08 18:25:48 +0100

    Configure webpack; initial commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      minify: false,
+      title: "Savvy CV",
+      template: "./src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "./public", to: path.resolve(__dirname, "dist") }],
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "[name][ext]",
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp|ico)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
