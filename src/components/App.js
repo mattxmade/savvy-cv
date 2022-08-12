61caeebabc7661fe422a8f757c396a17c4f500ba
Matt <97047080+mattxmade@users.noreply.github.com>
2022-08-12 17:52:09 +0100

    Move form components; remove unused functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
-import styled from "styled-components";
 
-import About from "./About";
-import Statement from "./Statement";
-import Education from "./Education";
-import Experience from "./Experience";
+import Nav from "./Nav";
+import Aside from "./Aside";
+import Form from "./pages/form/Form";
 
 class App extends Component {
   constructor(props) {
@@ -12,30 +10,24 @@ class App extends Component {
 
     this.state = {
       sections: [],
+      cv: {
+        about: {},
+        skills: [],
+        statement: "",
+        education: [],
+        experience: [],
+      },
     };
   }
-
-  componentDidMount() {
-    this.setState({
-      sections: [<About />, <Statement />, <Education />, <Experience />],
-    });
-  }
-  componentDidUpdate(prevProps, prevState) {}
 
   render() {
     return (
       <div className="wrapper">
-        <header>
-          <h1>Savvy CV</h1>
-          <p>A CV building app</p>
-        </header>
+        <Nav />
+        <Aside />
 
         <main>
-          <ul>
-            {this.state.sections.map((section, index) => (
-              <li key={index}>{section}</li>
-            ))}
-          </ul>
+          <Form />
         </main>
       </div>
     );
