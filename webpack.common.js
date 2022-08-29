45368916aec98b199e4e0d4920acd5e12fd2b1b9
Matt <97047080+mattxmade@users.noreply.github.com>
2022-08-29 08:47:47 +0100

    Create common webpack config file

---
+++ webpack.common.js
@@ -0,0 +1,73 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  resolve: {
+    fallback: {
+      module: "empty",
+      dgram: "empty",
+      dns: "mock",
+      fs: "empty",
+      http2: "empty",
+      net: "empty",
+      tls: "empty",
+      child_process: "empty",
+      process: require.resolve("process/browser"),
+      zlib: require.resolve("browserify-zlib"),
+      stream: require.resolve("stream-browserify"),
+      util: require.resolve("util"),
+      buffer: require.resolve("buffer"),
+      asset: require.resolve("assert"),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      minify: false,
+      title: "CV Savvy",
+      template: "./src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "./public", to: path.resolve(__dirname, "dist") }],
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+      process: "process/browser",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "[name][ext]",
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp|ico)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
