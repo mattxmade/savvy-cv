0df132b3c65c7e1687ec9c3bf0ec13d5d31b4085
Matt <97047080+mattxmade@users.noreply.github.com>
2022-08-29 08:12:04 +0100

    Build out form components; set state/handle props; set page index

--- src/components/pages/form/Form.js
+++ src/components/pages/form/Form.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 
 import About from "./parts/About";
+import Links from "./parts/Links";
+import Skills from "./parts/Skills";
 import Statement from "./parts/Statement";
 import Education from "./parts/Education";
 import Experience from "./parts/Experience";
@@ -10,38 +12,119 @@ class Form extends Component {
     super(props);
 
     this.state = {
-      sections: [],
+      index: 0,
 
-      form: {
-        statement: "",
-        skills: [],
-        education: [],
-        experience: [],
-        about: { first: "", last: "", email: "", telephone: "" },
+      fieldsetLockState: {
+        about: false,
+        links: false,
+        skills: false,
+        statement: false,
+        education: false,
+        experience: false,
       },
+
+      cvData: this.props.cvData,
     };
   }
 
   componentDidMount() {
-    this.setState({
-      sections: [
-        <About input={this.state.form.about} />,
-        <Statement input={this.state.form.statement} />,
-        <Education input={this.state.education} />,
-        <Experience input={this.state.experience} />,
-      ],
-    });
+    this.props.setPageIndex(0);
+
+    for (const [key, value] of Object.entries(this.state.fieldsetLockState)) {
+      this.updateIcons(key, value);
+    }
   }
 
-  componentDidUpdate(prevProps, prevState) {}
+  componentDidUpdate(prevProps, prevState) {
+    for (const [key, value] of Object.entries(this.state.fieldsetLockState)) {
+      this.updateIcons(key, value);
+    }
+  }
+
+  updateIcons = (fieldset, lockState) => {
+    const lockIcon = document.querySelector(`.state-${fieldset}`);
+    const mask = document.querySelector(`.fieldset-mask_${fieldset}`);
+
+    if (!lockState) {
+      lockIcon.classList.remove("fa-lock");
+      lockIcon.classList.add("fa-unlock");
+      mask.classList.remove("toggle-fieldset-mask");
+    }
+
+    if (lockState) {
+      lockIcon.classList.remove("fa-unlock");
+      lockIcon.classList.add("fa-lock");
+      mask.classList.add("toggle-fieldset-mask");
+    }
+  };
+
+  updateLockState = (fieldset, lockState) => {
+    const newFieldsetLockState = this.state.fieldsetLockState;
+    newFieldsetLockState[fieldset] = lockState;
+
+    this.setState({
+      fieldsetLockState: newFieldsetLockState,
+    });
+  };
 
   render() {
     return (
       <form>
         <ul className="form-ul">
-          {this.state.sections.map((section, index) => (
-            <li key={index}>{section}</li>
-          ))}
+          <li>
+            <div className="fieldset-mask fieldset-mask_about"></div>
+            <About
+              lockState={this.state.fieldsetLockState.about}
+              updateLockState={this.updateLockState}
+              input={this.props.cvData.about.content}
+              cb={this.props.updateCV}
+            />
+          </li>
+          <li>
+            <div className="fieldset-mask fieldset-mask_links"></div>
+            <Links
+              lockState={this.state.fieldsetLockState.links}
+              updateLockState={this.updateLockState}
+              input={this.props.cvData.links.content}
+              cb={this.props.updateCV}
+            />
+          </li>
+          <li>
+            <div className="fieldset-mask fieldset-mask_skills"></div>
+            <Skills
+              lockState={this.state.fieldsetLockState.skills}
+              updateLockState={this.updateLockState}
+              input={this.props.cvData.skills.content}
+              cb={this.props.updateCV}
+            />
+          </li>
+          <li>
+            <div className="fieldset-mask fieldset-mask_statement"></div>
+            <Statement
+              lockState={this.state.fieldsetLockState.statement}
+              updateLockState={this.updateLockState}
+              input={this.props.cvData.statement.content}
+              cb={this.props.updateCV}
+            />
+          </li>
+          <li>
+            <div className="fieldset-mask fieldset-mask_education"></div>
+            <Education
+              lockState={this.state.fieldsetLockState.education}
+              updateLockState={this.updateLockState}
+              input={this.props.cvData.education.content}
+              cb={this.props.updateCV}
+            />
+          </li>
+          <li>
+            <div className="fieldset-mask fieldset-mask_experience"></div>
+            <Experience
+              lockState={this.state.fieldsetLockState.experience}
+              updateLockState={this.updateLockState}
+              input={this.props.cvData.experience.content}
+              cb={this.props.updateCV}
+            />
+          </li>
         </ul>
       </form>
     );
