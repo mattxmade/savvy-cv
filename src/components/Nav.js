b5babec4e767aa415682457ecd5777181fd5f068
Matt <97047080+mattxmade@users.noreply.github.com>
2022-08-29 07:53:44 +0100

    Create nav components, setup react-router Links, handle props

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,70 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+const FormIcon = (props) => (
+  <li>
+    <i
+      aria-hidden="true"
+      className="far fa-edit"
+      onClick={() => props.navToPage(0)}
+    ></i>
+  </li>
+);
+
+const LayoutIcon = (props) => (
+  <li>
+    <i
+      aria-hidden="true"
+      className="far fa-save"
+      onClick={() => props.navToPage(1)}
+    ></i>
+  </li>
+);
+
+const PdfIcon = (props) => (
+  <li>
+    <i
+      aria-hidden="true"
+      className="far fa-file-pdf"
+      onClick={() => props.navToPage(2)}
+    ></i>
+  </li>
+);
+
+const ClearIcon = (props) => (
+  <li>
+    <i
+      aria-hidden="true"
+      className="far fa-trash-alt"
+      onClick={() => props.clear()}
+    ></i>
+  </li>
+);
+
+const linkStyle = {
+  color: "black",
+};
+
+const StandardNav = (props) => (
+  <ul>
+    <Link style={linkStyle} to="/">
+      <FormIcon navToPage={props.navToPage} />
+    </Link>
+    <Link style={linkStyle} to="/editor">
+      <LayoutIcon navToPage={props.navToPage} />
+    </Link>
+    <Link style={linkStyle} to="/viewer">
+      <PdfIcon navToPage={props.navToPage} />
+    </Link>
+    <ClearIcon clear={props.clear} />
+  </ul>
+);
 
 class Nav extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <nav>
@@ -8,18 +72,10 @@ class Nav extends Component {
           <h1>CV Savvy</h1>
           <p>A CV building app</p>
         </div>
-
-        <ul>
-          <li>
-            <i className="far fa-save"></i>
-          </li>
-          <li>
-            <i className="far fa-edit"></i>
-          </li>
-          <li>
-            <i className="far fa-trash-alt"></i>
-          </li>
-        </ul>
+        <StandardNav
+          navToPage={this.props.navToPage}
+          clear={this.props.clear}
+        />
       </nav>
     );
   }
