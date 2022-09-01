d4b153e0e83f9b71fa99ef990d9e6fb55f4ceb2c
Matt <97047080+mattxmade@users.noreply.github.com>
2022-09-01 11:35:00 +0100

    Move Editor components, modifiy formatLayout class method

--- src/components/pages/editor/Editor.js
+++ src/components/pages/editor/Editor.js
@@ -1,146 +1,7 @@
 import React, { Component } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-const About = (content) => (
-  <ul className="format-ab">
-    {content.name ? (
-      <li>
-        <p>{content.name}</p>
-      </li>
-    ) : (
-      <p />
-    )}
-
-    <li>
-      {content.email ? (
-        <p>
-          <span>
-            <i className="fas fa-at"></i>
-          </span>
-          {content.email}
-        </p>
-      ) : (
-        ""
-      )}
-
-      {content.telephone ? (
-        <p>
-          <span style={{ left: 4 }}>
-            <i className="fas fa-mobile-alt"></i>
-          </span>
-          {content.telephone}
-        </p>
-      ) : (
-        ""
-      )}
-
-      {content.portfolio ? (
-        <p style={{ marginTop: 25 }}>
-          <span style={{ left: 3 }}>
-            <i className="fas fa-portrait"></i>
-          </span>
-          {content.portfolio}
-        </p>
-      ) : (
-        ""
-      )}
-    </li>
-  </ul>
-);
-
-const validIcon = (type) => {
-  switch (type) {
-    case "Twitter":
-      return <i aria-hidden="true" className="fab fa-twitter-square"></i>;
-    case "Instagram":
-      return <i aria-hidden="true" className="fab fa-instagram-square"></i>;
-    case "GitHub":
-      return <i aria-hidden="true" className="fab fa-github-square"></i>;
-    default:
-      return <i aria-hidden="true" className="fas fa-globe"></i>;
-  }
-};
-
-const Links = (content) => (
-  <ul className="format-lk">
-    {content.map((link, index) => {
-      const icon = validIcon(link.type);
-      return (
-        <li key={index}>
-          <p>{icon ? icon : link.type + " "}</p>
-          <span>{link.url}</span>
-        </li>
-      );
-    })}
-  </ul>
-);
-
-const Skills = (content) => (
-  <ul className="format-sk">
-    {content.map((skill, index) => (
-      <li key={index}>
-        <p>{skill}</p>
-      </li>
-    ))}
-  </ul>
-);
-
-const Statement = (content) => <p className="format-st">{content}</p>;
-
-const Education = (content) => (
-  <ul className="format-ed-list">
-    {content.map((knowledge, index) => {
-      return (
-        <li className="format-ed-item" key={index}>
-          <ul className="format-ql-list">
-            {knowledge.quals.map((qual, index) => (
-              <li className="format-ql-item" key={index}>
-                <p>{qual}</p>
-              </li>
-            ))}
-          </ul>
-
-          <ul>
-            <li>
-              <h3>{knowledge.institute}</h3>
-            </li>
-            <li className="format-ed-date">
-              <p>{knowledge.date.start}</p>
-              <p>{knowledge.date.end}</p>
-            </li>
-          </ul>
-        </li>
-      );
-    })}
-  </ul>
-);
-
-const Experience = (content) => (
-  <ul className="format-xp-list">
-    {content.map((xperience, index) => (
-      <li className="format-xp-item" key={index}>
-        <ul className="format-xp-job">
-          <li className="job-title-li-cont">
-            <h3>{xperience.job.title}</h3>
-          </li>
-          <li>
-            <p>{xperience.job.description}</p>
-          </li>
-        </ul>
-
-        <ul>
-          <li>
-            <h3>{xperience.employer}</h3>
-          </li>
-          <li className="format-xp-date">
-            <p>{xperience.date.start}</p>
-            <p>{xperience.date.end}</p>
-          </li>
-        </ul>
-      </li>
-    ))}
-  </ul>
-);
+import Segment from "./Segment";
 
 class Editor extends Component {
   constructor(props) {
@@ -148,6 +9,7 @@ class Editor extends Component {
 
     this.state = {
       index: 1,
+      pdfComponents: this.props.cvComponents,
     };
   }
 
@@ -174,17 +36,17 @@ class Editor extends Component {
   formatLayout = (component) => {
     switch (component.section) {
       case "about":
-        return About(component.content);
+        return Segment.About(component.content);
       case "links":
-        return Links(component.content);
+        return Segment.Links(component.content);
       case "skills":
-        return Skills(component.content);
+        return Segment.Skills(component.content);
       case "statement":
-        return Statement(component.content);
+        return Segment.Statement(component.content);
       case "education":
-        return Education(component.content);
+        return Segment.Education(component.content);
       case "experience":
-        return Experience(component.content);
+        return Segment.Experience(component.content);
     }
   };
 
@@ -202,12 +64,18 @@ class Editor extends Component {
     );
 
     this.props.updateCvIndex(mapToArray);
+    this.setState({ pdfComponents: mapToArray });
   };
 
   render() {
     return (
       <div className="editor">
-        <h2>CV Format Editor</h2>
+        <h2>
+          <i className="fas fa-align-left"></i> CV Layout Editor
+        </h2>
+        <span className="instructions">
+          drag and drop sections to change layout
+        </span>
         <DragDropContext onDragEnd={this.handleOnDragEnd}>
           <Droppable droppableId="editor-sections">
             {(provided) => (
@@ -217,7 +85,7 @@ class Editor extends Component {
                 ref={provided.innerRef}
               >
                 {this.props.cvComponents.map((component, index) => {
-                  if (!this.checkContent(component)) return;
+                  if (!this.checkContent(component)) return null;
 
                   return (
                     <Draggable
